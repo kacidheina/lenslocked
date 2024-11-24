0972728698855405790487cca5a5f323a02a0f42
Ina Kacidhe <inakacidhe@Inas-MacBook-Pro.local>
2024-11-24 22:39:07 +0100

    Rendering the faq page via a template

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
@@ -26,32 +26,15 @@ func executeTemplate(w http.ResponseWriter, filepath string) {
 }
 
 func homeHandler(w http.ResponseWriter, req *http.Request) {
-	executeTemplate(w, "templates/home.gohtml")
+	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
 }
 
 func contactHandler(w http.ResponseWriter, req *http.Request) {
-	executeTemplate(w, "templates/contact.gohtml")
+	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
 }
 
 func faqHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-<ul>
-  <li>
-    <b>Is there a free version?</b>
-    Yes! We offer a free trial for 30 days on any paid plans.
-  </li>
-  <li>
-    <b>What are your support hours?</b>
-    We have support staff answering emails 24/7, though response
-    times may be a bit slower on weekends.
-  </li>
-  <li>
-    <b>How do I contact support?</b>
-    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-  </li>
-</ul>
-`)
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func galleryHandler(w http.ResponseWriter, req *http.Request) {
@@ -62,7 +45,6 @@ func galleryHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 
 	r.Get("/", homeHandler)
 	r.Get("/galleries/{id}", galleryHandler)
